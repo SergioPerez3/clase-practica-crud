7931b1d380559b685d552ec3e86b9d9bbd57e551
Sergio Perez <Sergio_01_3@hotmail.com>
2026-03-10 20:14:19 +0100

    Repaso API

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import "./db.js"
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.json({message: "Bienvenidos a la API"});
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
+
